e51983958d068b4da89a22f779580749f9ddd200
jungx1234 <88120930+jungx1234@users.noreply.github.com>
2021-08-08 18:30:34 +0900

    Add files via upload

---
+++ edwith10-2.c
@@ -0,0 +1,14 @@
+﻿int main()
+{
+	int i;
+	double sum;
+
+	sum = 0;
+	for (i = 1; i <= 100; i++)
+		sum = sum + (1.0 / i);
+
+	printf("1/1+1/2+1/3＋1?4+・・・+1/100=%.2lf\n", n, sum);
+
+	return 0;
+
+}
